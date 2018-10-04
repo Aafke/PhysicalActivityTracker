7dd906a641b68d6aa67e5f7d7e6f03a2490d91e6
Aafke Meints <aafkegroenveld@hotmail.com>
2018-10-04 10:43:19 +0200

    added table with records in homeview

--- Activity/models.py
+++ Activity/models.py
@@ -5,9 +5,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-def get_default_user():
-    return User.objects.get(id=1)
+class ActivityRecordQuerySet(models.QuerySet):
+    def activity_records_for_user(self, user):
 
+        return self.filter(user=user)
 
 """ ActivityRecord consist of a type of activity and the duration of that activity"""
 # todo: duration in hours and minutes
@@ -18,6 +19,9 @@ class ActivityRecord(models.Model):
     activity_type = models.CharField(max_length=300)
     duration = models.IntegerField(default=0)
 
+    objects = ActivityRecordQuerySet.as_manager()
+
     def __str__(self):
-        return "{0}   {1}   {2}".format(self.user, self.activityType, self.duration)
+        return "{0}   {1}   {2}".format(self.user, self.activity_type, self.duration)
+
 
--- UserPage/views.py
+++ UserPage/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+from Activity.models import ActivityRecord
+
 
 def home(request):
-    return render(request, "UserPage/home.html")
+    number_of_user_records = ActivityRecord.objects.activity_records_for_user(request.user).count()
+    user_records = ActivityRecord.objects.activity_records_for_user(request.user)
+
+    return render(request, "UserPage/home.html",
+                  {'nrecords': number_of_user_records,
+                   'records': user_records})
